bf0cc617863b647cf97a019db2706739d3a8c981
gursewak96 <46025020+gursewak96@users.noreply.github.com>
2019-01-19 17:10:30 +0530

    Edge Detection function
    Implementation of Edge detection algorithm using a convolution matrix or mask.

--- EdgeDetection.cpp
+++ EdgeDetection.cpp
@@ -5,12 +5,11 @@
 using namespace cv;
 using namespace std;
 
-void Sharpen(const Mat &, Mat&);
 void EdgeDetection(const Mat& I, Mat & R);
 
 static void help() {
 	cout << endl
-		<< "This program shows how much time is taked by edge detection implementation."
+		<< "This program shows how much time is taken by edge detection implementation."
 		<< endl;
 }
 
@@ -38,7 +37,7 @@ int main(int argc, char* argv[]){
 	 
 	/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
-	//Measuring time for Sharpen function
+	//Measuring time for Edge Detection function
 	Mat _I = M.clone();
 
 	
@@ -61,32 +60,6 @@ int main(int argc, char* argv[]){
 	return 0;
 }
 
-
-
-
-
-void Sharpen(const Mat& I, Mat & R) {
-	CV_Assert(I.depth() == CV_8U);
-
-	const int nChannels = I.channels();
-	R.create(I.size(), I.type());
-
-	for (int i = 1; i < I.rows - 1; i++) {
-		const uchar * prev = I.ptr<uchar>(i - 1);
-		const uchar * current = I.ptr<uchar>(i);
-		const uchar * next = I.ptr<uchar>(i + 1);
-
-		uchar * output = R.ptr<uchar>(i);
-		for (int j = nChannels; j < nChannels*(I.cols - 1); j++) {
-			*output++ = saturate_cast<uchar>(5 * current[j] - current[j - nChannels] - current[j + nChannels] - prev[j] - next[j]);
-		}
-	}
-
-	R.row(0).setTo(Scalar(0));
-	R.row(R.rows - 1).setTo(Scalar(0));
-	R.col(0).setTo(Scalar(0));
-	R.col(R.cols - 1).setTo(Scalar(0));
-}
 
 void EdgeDetection(const Mat& I, Mat & R) {
 	CV_Assert(I.depth() == CV_8U);
